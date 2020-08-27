ccae8398407b19d8933487fe327a2b7934fbf148
dedalus <44595163+smzdmjjdd@users.noreply.github.com>
2020-08-27 21:41:07 +0800

    Add files via upload

---
+++ value based decision making/risky choices/risky_choice_model.m
@@ -0,0 +1,41 @@
+%% Risky decision making model
+% From Rutledge, R. B., et al. (2015). "Dopaminergic Modulation of Decision Making and Subjective Well-Being." Journal of Neuroscience 35(27): 9811-9822.
+
+% -c is the subject's choices,1 for certain choice, 2 for gamble choice
+% -cr is the value of certain choices
+% -ur is the value of uncertain choices, which includes two columns.The
+% first column is the bigger possibility and the second column is the
+% smaller possibility.
+% -ag is the utility function parameter of gain 
+% -al is the utility function parameter of loss
+% -lambda is the loss aversion parameter
+% -beta is the inverse temperature parameter
+
+function ll=risky_choice_model(c,cr,ur,ag,al,lambda,beta)
+trial_number=length(c);
+%calculate the gamble choices' utility.
+u_gamble=0.5.*ur(:,1).^ag-0.5.*lambda.*(-ur(:,2)).^al;
+
+%calculate the certain choices' utility.
+for trial=1:trial_number
+    if cr(trial)>=0
+        u_certain(trial)=cr^ag;
+    elseif cr(trial)<0
+        u_certain(trial)=-lambda*(-cr)^al;
+    end
+end
+%softmax choice kernel
+proba_gamble=1./(1+exp(-beta.*(u_gamble-u_certain)));
+proba_certain=1-proba_gamble;
+proba_sum=[proba_certain;proba_gamble];
+
+%combine the real data with model data
+for trial=1:trial_number
+    single_ll(trial)=proba_sum(trial,c(trial));
+end
+
+ll=-sum(log(single_ll));
+
+ 
+
+
