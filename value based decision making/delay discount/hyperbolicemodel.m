06d68e92c24340bf764103141dab621ac40ac9d9
dedalus <44595163+smzdmjjdd@users.noreply.github.com>
2020-08-28 00:38:17 +0800

    Update hyperbolicemodel.m

--- value based decision making/delay discount/hyperbolicemodel.m
+++ value based decision making/delay discount/hyperbolicemodel.m
@@ -1,4 +1,6 @@
-%% A simple hyperbolice model for practice.
+%% A simple hyperbolic model for practice.
+%% From Kable, J. W. and P. W. Glimcher (2007). "The neural correlates of subjective value during intertemporal choice." Nat Neurosci 10(12): 1625-1633.
+
 %% parameter 'c' : a vector,subject's real choice,1 represents sooner smaller choice,2 represents later larger choice
 %% parameter 'r' : a two column matrix,the first column is the sooner smaller reward whereas the second column is the larger later reward
 %% parameter 't' : a two column matrix,the first column is the sooner smaller time whereas the second column is the larger later time
