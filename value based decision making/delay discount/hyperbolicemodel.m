b2d4bc5d0180be76b799091ee47c1145a3524a18
dedalus <44595163+smzdmjjdd@users.noreply.github.com>
2020-11-27 04:06:39 +0800

    Update hyperbolicemodel.m

--- value based decision making/delay discount/hyperbolicemodel.m
+++ value based decision making/delay discount/hyperbolicemodel.m
@@ -6,8 +6,9 @@
 %% parameter 't' : a two column matrix,the first column is the sooner smaller time whereas the second column is the larger later time
 %% parameter 'k' : discount rate
 %% parameter 'beta' : inverse temperature parameter
+%% parameter 'e' : epsilon greedy parameter, interpreted as motor noise
 
-function ll=hyperbolicemodel(c,r,t,k,beta)
+function ll=hyperbolicemodel(c,r,t,k,beta,e)
 
 T=length(c);
 sv=r./(k.*t+1);
@@ -16,6 +17,7 @@ delta_sv(:,2)=sv(:,2)-sv(:,1);
 proba=1./(1+exp(-beta.*delta_sv));
 for trial=1:T
     single_ll(trial)=proba(trial,c(trial));
+single_ll=single_ll*(1-e)+e/2;  %% two choices, therefore the random selection probability is e/2.
 end
 ll=-sum(log(single_ll));
     
